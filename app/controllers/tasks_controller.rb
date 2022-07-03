0ecc09cf2f1fbb0d816ab43af02e3e6e6b2e94e2
glassjohnny <good.bye.betelgeuse0723@gmail.com>
2022-07-03 16:34:10 +0900

    finish capter 4 & install Rspec gem

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     # すべてのタスクデータを取得し、@taskインスタンスメソッドへ代入
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,27 +14,28 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   def create
     # task_paramsで取得したパラメータを変数taskに代入
-    task = Task.new(task_params)
-    task.save!
+    @task = current_user.tasks.new(task_params)
+
+    if @task.save
     # redirect_toにflashメッセージを渡している
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました"
+    redirect_to @task, notice: "タスク「#{@task.name}」を登録しました"
+    else
+      render :new
+    end
   end
 
   private
@@ -42,5 +44,10 @@ class TasksController < ApplicationController
     # フォームから送信されたデータが想定どおりのデータの形であることをチェック
     # 想定通りのデータのみを取得
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    # idパラメータからタスクオブジェクトを検索して@taskに代入する
+    @task = current_user.tasks.find(params[:id])
   end
 end
